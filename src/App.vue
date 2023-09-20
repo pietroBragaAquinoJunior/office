b385c07c6e89caf33a7e75e6db866b541c586471
Piêtro <pietrobragaprofissional@gmail.com>
2023-09-19 21:08:11 -0300

    Mais coisas...

--- src/App.vue
+++ src/App.vue
@@ -37,8 +37,10 @@
     <div class="principal-section">
       <div style="display: flex; justify-content: center; flex-direction: column; align-items: center;">
         <img src="../public/microsoft-icon.svg" alt="svg-logo"/>
-        <h1 style="padding-top: 40px; font-size: 50px; font-weight: 500" class="texto-especial">Office é agora Microsoft 365</h1>
-        <p style="padding-top: 20px; font-size: 20px">O novo Microsoft 365 permite que você crie, compartilhe e <br/> colabore em um só lugar com seus aplicativos
+        <h1 style="padding-top: 40px; font-size: 50px; font-weight: 500" class="texto-especial">Office é agora Microsoft
+          365</h1>
+        <p style="padding-top: 20px; font-size: 20px">O novo Microsoft 365 permite que você crie, compartilhe e <br/>
+          colabore em um só lugar com seus aplicativos
           favoritos</p>
         <div style="display: flex; gap:30px; padding-top: 30px;">
           <button class="primary-button">Entrar</button>
@@ -47,7 +49,87 @@
         <p class="aviso-azul">Inscreva-se para obter a versão gratuita do Microsoft 365 ></p>
       </div>
       <div style="display: flex; justify-content: center">
-        <img style="border-radius: 20px" width="100%" src="./assets/imagem-principal.png" alt="Imagem Principal" />
+        <img style="border-radius: 20px" width="100%" src="./assets/imagem-principal.png" alt="Imagem Principal"/>
+      </div>
+    </div>
+  </section>
+  <section class="segoeui">
+    <div class="secondary-section">
+      <div class="secondary-section-menu">
+        <div class="card-button active">Para Página Inicial</div>
+        <div class="card-button">Para Organizações</div>
+        <div class="card-button">Para Educadores</div>
+      </div>
+      <div class="secondary-section-principal">
+        <div
+            style="display: flex; justify-content: center; flex-direction: column; align-items: center; text-align: center; gap: 30px">
+          <h2 class="h2-secondary-section">Gratuito ou premium: <br/>
+            o Microsoft 365 está com você</h2>
+          <p class="p-secondary-section">Todos obtém armazenamento em nuvem e aplicativos essenciais do <br/> Microsoft
+            365 na Web, gratuitamente</p>
+        </div>
+        <div>
+          <img src="./assets/first-secondary-section.png" alt="office image"/>
+        </div>
+        <div
+            style="display: flex; align-items: center; justify-content: space-between; width: 100%; padding-left: 100px; padding-right: 100px">
+          <div style="width: 500px; display: flex; flex-direction: column">
+            <img src="../public/plusicon.svg" alt="plus-icon" style="width: 50px"/>
+            <h2 class="h2-secondary-section">Criar algo inspirador</h2>
+            <p class="p-secondary-section" style="padding-top: 30px">Crie rapidamente qualquer coisa para você e sua
+              família. Cartões de aniversário, folhetos escolares, orçamentos, postagens sociais, vídeos e mais. nenhuma
+              experiência de design gráfico necessária.</p>
+            <p class="aviso-laranja">Inscreva-se para obter a versão gratuita do Microsoft 365 ></p>
+          </div>
+          <div>
+            <img alt="office image2" src="./assets/second-secondary-section.png"/>
+          </div>
+        </div>
+        <div
+            style="display: flex; align-items: center; justify-content: space-between; width: 100%; padding-left: 100px; padding-right: 100px">
+          <div>
+            <img alt="office image2" src="./assets/third-secondary-section.png"/>
+          </div>
+          <div style="width: 500px; display: flex; flex-direction: column">
+            <img src="../public/cloud-icon.svg" alt="plus-icon" style="width: 50px"/>
+            <h2 class="h2-secondary-section">Armazenar com confiança</h2>
+            <p class="p-secondary-section" style="padding-top: 30px">Seus arquivos e memórias permanecem seguros na
+              nuvem, com 5 GB gratuitos e mais de 1 TB se você se tornar premium.</p>
+          </div>
+        </div>
+        <div
+            style="display: flex; align-items: center; justify-content: space-between; width: 100%; padding-left: 100px; padding-right: 100px">
+          <div style="width: 500px; display: flex; flex-direction: column">
+            <img src="../public/arrow-icon.svg" alt="plus-icon" style="width: 50px"/>
+            <h2 class="h2-secondary-section">Compartilhar com <br/> amigos...</h2>
+            <p class="p-secondary-section" style="padding-top: 30px">... mesmo que eles não tenham o Microsoft 365.
+              Colabore e crie arquivos perfeitamente com seus amigos e familiares</p>
+          </div>
+          <div>
+            <img alt="office image2" src="./assets/five-secondary-section.png"/>
+          </div>
+        </div>
+        <div style="display: flex; align-items: center; justify-content: space-between; width: 100%; padding-left: 100px; padding-right: 100px">
+          <div>
+            <img alt="office image2" src="./assets/six-secondary-section.png"/>
+          </div>
+          <div style="width: 500px; display: flex; flex-direction: column">
+            <img src="../public/heart-icon.svg" alt="plus-icon" style="width: 50px"/>
+            <h2 class="h2-secondary-section">Proteja seus entes queridos</h2>
+            <p class="p-secondary-section" style="padding-top: 30px">Proteja seus entes queridos com recursos de
+              segurança digital e física e o aplicativo Proteção para a Família</p>
+          </div>
+        </div>
+        <div
+            style="display: flex; align-items: center; justify-content: space-between; width: 100%; padding-left: 100px;">
+          <div style="width: 500px; display: flex; flex-direction: column">
+            <h2 class="h2-secondary-section">Mais aplicativos em menos locais</h2>
+            <p class="p-secondary-section" style="padding-top: 30px">O novo Microsoft 365 reúne seus aplicativos Microsoft favoritos em uma única plataforma intuitiva</p>
+          </div>
+          <div>
+            <img alt="office image2" src="./assets/seven-secondary-section.png"/>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -136,7 +218,7 @@
 
 .principal-section {
 
-  background: linear-gradient(0deg, white, #E3E9FC );
+  background: linear-gradient(0deg, white, #E3E9FC);
   display: flex;
   flex-direction: column;
   gap: 50px;
@@ -152,7 +234,7 @@
   font-weight: bold; /* Aplica o negrito ao texto */
 }
 
-.primary-button{
+.primary-button {
   background: #464FEB;
   color: white;
   width: 205px;
@@ -163,10 +245,12 @@
   height: 40px;
   font-size: 15px;
 }
-.primary-button:hover{
+
+.primary-button:hover {
   background: #353cb2;
 }
-.secondary-button{
+
+.secondary-button {
   background: white;
   color: #464FEB;
   width: 205px;
@@ -177,16 +261,92 @@
   border: 2px solid #464FEB;
   font-size: 15px;
 }
-.secondary-button:hover{
+
+.secondary-button:hover {
   background: #d4d5ff;
 }
-.aviso-azul{
+
+.aviso-azul {
   margin-top: 25px;
   color: #464FEB;
   cursor: pointer;
   border-bottom: 2px solid transparent;
 }
-.aviso-azul:hover{
+
+.aviso-azul:hover {
   border-bottom: 2px solid #464FEB;
+}
+
+.secondary-section {
+  background: #FFF4ED;
+  min-height: 2000px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding-top: 45px;
+  padding-bottom: 45px;
+}
+
+.secondary-section-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  width: 92%;
+}
+
+.card-button {
+  height: 130px;
+  flex: 1;
+  font-weight: 500;
+  font-size: 20px;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #792000;
+  background: #F9DBD0;
+  box-shadow: #fff 0 8px 0, rgba(0, 0, 0, .14) 0 0 8px;
+}
+
+.card-button.active {
+  background: white;
+  color: #D83B01;
+}
+
+.secondary-section-principal {
+  min-height: 1000px;
+  background: white;
+  width: 92%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  gap: 100px;
+}
+
+.h2-secondary-section {
+  font-size: 37px;
+  color: #242424;
+  font-weight: 500;
+}
+
+.p-secondary-section {
+  font-size: 18px
+}
+
+.aviso-laranja {
+  margin-top: 25px;
+  color: #D83B01;
+  cursor: pointer;
+  border-bottom: 2px solid transparent;
+}
+
+.aviso-laranja:hover {
+  border-bottom: 2px solid #D83B01;
 }
 </style>
